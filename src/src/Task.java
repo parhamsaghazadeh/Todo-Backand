defc7ea08046a87f062f3eda5dad69a8ba6c77b1
parham <parhamsaghazadeh@gmail.com>
2025-09-24 14:51:01 +0330

    add attributes

--- src/src/Task.java
+++ src/src/Task.java
@@ -1,2 +1,42 @@
 public class Task {
+    private int id;
+    private String title;
+    private boolean done;
+
+    public Task(int id, String title, boolean done) {
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean getDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String toString() {
+        return id + ": " + title + " [" + (done ? "✓" : "✗") + "]";
+
+    }
 }
+
+
